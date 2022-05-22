b89b45a853b8c96e87dba1e1a1af82b2bded6541
marawan20191700616@cis.asu.edu.eg <Marosoma141>
2022-05-22 07:45:59 +0200

    final with test cases

--- HPC_ProjectTemplate/Source.cpp
+++ HPC_ProjectTemplate/Source.cpp
@@ -20,7 +20,6 @@ int* inputImage(int* w, int* h, String^ imagePath) //put the size of image in w 
 {
 	int* input;
 	int OriginalImageWidth, OriginalImageHeight;
-
 	//*********************************************************Read Image and save it to local arrayss*************************	
 	//Read Image and save it to local arrayss
 	Bitmap BM(imagePath);
@@ -38,22 +37,18 @@ int* inputImage(int* w, int* h, String^ imagePath) //put the size of image in w 
 		for (int j = 0; j < BM.Width; j++)
 		{
 			Color c = BM.GetPixel(j, i);
-
 			Red[i * BM.Width + j] = c.R;
 			Blue[i * BM.Width + j] = c.B;
 			Green[i * BM.Width + j] = c.G;
-
-			input[i * BM.Width + j] = ((c.R + c.B + c.G) / 3); //gray scale value equals the average of RGB values
-			//cout << input[i * BM.Width + j] << " ";
+			input[i * BM.Width + j] = ((c.R + c.B + c.G) / 3);
 		}
-		//cout << i << endl;
-
 	}
 	return input;
 }
 void createImage(int* image, int width, int height, int index)
 {
-	
+	int start_s, stop_s, TotalTime = 0;
+	start_s = clock();
 	MPI_Init(NULL, NULL);
 	int size, rank;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -90,10 +85,8 @@ void createImage(int* image, int width, int height, int index)
 				y_mover++;
 			}
 			res = res / 9;
-			Color c = Color::FromArgb(res, res, res);	
+			Color c = Color::FromArgb(res, res, res);
 			MyNewImage.SetPixel(x, y, c);
-
-			
 		}
 		MPI_Status status;
 		for (int i = 1; i < size; i++) {
@@ -110,10 +103,12 @@ void createImage(int* image, int width, int height, int index)
 		}
 		MyNewImage.Save("..//Data//Output//outputRes" + index + ".png");
 		cout << "result Image Saved " << index << endl;
+		stop_s = clock();
+		TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000; // seq 3473 
+		cout << "time: " << TotalTime << endl;
 	}
 	else
 	{
-		//cout << "hello from rank" << rank << ' ';
 		int* imageData = new int[MyNewImage.Height * MyNewImage.Width];
 		MPI_Status status;
 		MPI_Recv(imageData, MyNewImage.Height * MyNewImage.Width, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
@@ -142,44 +137,24 @@ void createImage(int* image, int width, int height, int index)
 			}
 			res = res / 9;
 			imageData[y * MyNewImage.Width + x] = res;
-			// mask ends here 
-			//Color c = Color::FromArgb(res, res, res);	
-			//MyNewImage.SetPixel(x, y, c);
-
-
 		}
 		MPI_Send(imageData, MyNewImage.Width * MyNewImage.Height, MPI_INT, 0, 0, MPI_COMM_WORLD);
 	}
-	
 	MPI_Finalize();
-	
-	
 }
 
 
 int main()
 {
 	int ImageWidth, ImageHeight;
-
-	
-
 	String^ imagePath;
 	string img;
-	img = "..//Data//Input//test.png";
-
+	img = "..//Data//Input//N.png";
 	imagePath = marshal_as<String^>(img);
 	int* imageData = inputImage(&ImageWidth, &ImageHeight, imagePath);
-
-
-	int start_s, stop_s, TotalTime = 0;
-	start_s = clock();
 	createImage(imageData, ImageWidth, ImageHeight, 0);
 	free(imageData);
-	stop_s = clock();
-	TotalTime += (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000; // seq 3473 
-	cout << "time: " << TotalTime << endl;
 	return 0;
-
 }
 
 
