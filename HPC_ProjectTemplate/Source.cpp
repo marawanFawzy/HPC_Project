91db506900bd168db300d3349a762fdf120f6235
marawan20191700616@cis.asu.edu.eg <Marosoma141>
2022-05-23 04:53:55 +0200

    refactor

--- HPC_ProjectTemplate/Source.cpp
+++ HPC_ProjectTemplate/Source.cpp
@@ -48,36 +48,30 @@ int* inputImage(int* w, int* h, String^ imagePath) //put the size of image in w 
 int start_s, stop_s, TotalTime = 0;
 void createImage(int* image, int width, int height, int index)
 {
-	
-	
-	
+
+
+
 	int size, rank;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	//cout << "rank : " << rank << endl;
-	//cout << "size : " << size << endl;
 	Bitmap MyNewImage(width, height);
-	//180 // 180 1
+	//180 // 186
 	if (rank == 0)
 	{
 		for (int i = 1; i < size; i++)
-			MPI_Send(image, MyNewImage.Width * MyNewImage.Height, MPI_INT, i, 0, MPI_COMM_WORLD);
-		int start = rank * ((MyNewImage.Height * MyNewImage.Width) / size);
-		int end = (rank + 1) * ((MyNewImage.Height * MyNewImage.Width) / size);
+			MPI_Send(image, width * height, MPI_INT, i, 0, MPI_COMM_WORLD);
+		int start = rank * ((height * width) / size);
+		int end = (rank + 1) * ((height * width) / size);
 		for (int k = start; k < end; k++)
 		{
-			int res = 0;
-			int y = k / MyNewImage.Width;
-			int x = k % MyNewImage.Width;
-			int x_mover = -1;
-			int y_mover = -1;
+			int res = 0, y = k / width , x = k % width , x_mover = -1 , y_mover = -1 , counter = 9;
 			for (int i = 0; i < 3; i++)
 			{
 				for (int j = 0; j < 3; j++)
 				{
 					if ((y + y_mover < 0) || (x + x_mover < 0) || (y + y_mover >= MyNewImage.Height) || (x + x_mover >= MyNewImage.Width))
 					{
-						res = res + 0;
+						counter--;
 					}
 					else res += image[(y + y_mover) * width + (x + x_mover)];
 					x_mover++;
@@ -85,19 +79,19 @@ void createImage(int* image, int width, int height, int index)
 				x_mover = -1;
 				y_mover++;
 			}
-			res = res / 9;
+			res = res / counter;
 			Color c = Color::FromArgb(res, res, res);
 			MyNewImage.SetPixel(x, y, c);
 		}
 		MPI_Status status;
 		for (int i = 1; i < size; i++) {
-			MPI_Recv(image, MyNewImage.Height * MyNewImage.Width, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
-			int start = i * ((MyNewImage.Height * MyNewImage.Width) / size);
-			int end = (i + 1) * ((MyNewImage.Height * MyNewImage.Width) / size);
+			MPI_Recv(image, height * width, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+			int start = i * ((height * width) / size);
+			int end = (i + 1) * ((height * width) / size);
 			for (int k = start; k < end; k++)
 			{
-				int y = k / MyNewImage.Width;//i
-				int x = k % MyNewImage.Width;//j
+				int y = k / width;
+				int x = k % width;
 				Color c = Color::FromArgb(image[k], image[k], image[k]);
 				MyNewImage.SetPixel(x, y, c);
 			}
@@ -110,38 +104,36 @@ void createImage(int* image, int width, int height, int index)
 	}
 	else
 	{
-		int* imageData = new int[MyNewImage.Height * MyNewImage.Width];
+		int* imageData = new int[height * width];
 		MPI_Status status;
-		MPI_Recv(imageData, MyNewImage.Height * MyNewImage.Width, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-		int start = rank * ((MyNewImage.Height * MyNewImage.Width) / size);
-		int end = (rank + 1) * ((MyNewImage.Height * MyNewImage.Width) / size);
+		MPI_Recv(imageData, height * width, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		int start = rank * ((height * width) / size);
+		int end = (rank + 1) * ((height * width) / size);
 		for (int k = start; k < end; k++)
 		{
-			int res = 0;
-			int y = k / MyNewImage.Width;//i
-			int x = k % MyNewImage.Width;//j
-			int x_mover = -1;
-			int y_mover = -1;
+			int res = 0, y = k / width, x = k % width , x_mover = -1 , y_mover = -1 , counter = 9;
 			for (int i = 0; i < 3; i++)
 			{
 				for (int j = 0; j < 3; j++)
 				{
 					if ((y + y_mover < 0) || (x + x_mover < 0) || (y + y_mover >= MyNewImage.Height) || (x + x_mover >= MyNewImage.Width))
 					{
-						res = res + 0;
+						counter--;
 					}
-					else res += image[(y + y_mover) * width + (x + x_mover)];
+					else
+						res += image[(y + y_mover) * width + (x + x_mover)];
 					x_mover++;
 				}
 				x_mover = -1;
 				y_mover++;
+
 			}
-			res = res / 9;
-			imageData[y * MyNewImage.Width + x] = res;
+			res = res / counter;
+			imageData[y * width + x] = res;
 		}
-		MPI_Send(imageData, MyNewImage.Width * MyNewImage.Height, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(imageData, width * height, MPI_INT, 0, 0, MPI_COMM_WORLD);
 	}
-	
+
 }
 
 
@@ -150,12 +142,12 @@ int main()
 	int ImageWidth, ImageHeight;
 	String^ imagePath;
 	string img;
-	img = "..//Data//Input//5N.png";
+	img = "..//Data//Input//N.png";
 	imagePath = marshal_as<String^>(img);
 	int* imageData = inputImage(&ImageWidth, &ImageHeight, imagePath);
 	start_s = clock();
 	MPI_Init(NULL, NULL);
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 1; i++)
 	{
 		MPI_Barrier(MPI_COMM_WORLD);
 		createImage(imageData, ImageWidth, ImageHeight, i);
